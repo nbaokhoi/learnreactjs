e046613cfc3c27a7bca906a9c5d1a689185129ca
Khoi Nguyen [Nguyen Bao Khoi] <nbkhoi@icloud.com>
2018-02-23 15:52:36 +0700

    Components Example Commit

--- App.jsx
+++ App.jsx
@@ -1,13 +1,68 @@
 import React from 'react';
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [
+                {
+                    "id": 1,
+                    "name": "Foo",
+                    "age": 20
+                },
+                {
+                    "id": 2,
+                    "name": "Bar",
+                    "age": 22
+                },
+                {
+                    "id": 3,
+                    "name": "Baz",
+                    "age": 23
+                }
+            ]
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Header />
+                <Content data = {this.state.data} />
+            </div>
+        );
+    }
+}
+class Header extends React.Component {
     render() {
         return (
             <div>
                 <h1>Header</h1>
-                <h2>Content</h2>
-                <p>This is the content!!!</p>
-                <p>{1+1}</p>
             </div>
+        );
+    }
+}
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Content</h2>
+                <p>The content text!!!</p>
+                <table>
+                    <tbody>
+                        {this.props.data.map((person, i) => <TableRow key={i} data={person} />)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+class TableRow extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.data.id}</td>
+                <td>{this.props.data.name}</td>
+                <td>{this.props.data.age}</td>
+            </tr>
         );
     }
 }
